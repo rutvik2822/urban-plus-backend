7fec78530f917940683e9b5ad5ba6365316fed94
rutvik2822 <rutvikdevdare@gmail.com>
2025-10-01 13:15:35 +0530

    Updated server.js with env vars for DB connection

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 // MySQL connection (✅ keep ONLY env variables for deployment)
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
